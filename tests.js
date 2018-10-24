c9249e72b0ec171c9b26e42c0f15ca57fac894a9
nitrojs <n1trojscoding@mail.ru>
2018-10-24 21:13:45 +0300

    tests.js: added search.user method

--- tests.js
+++ tests.js
@@ -3,4 +3,9 @@ const { search } = require('./lib/index');
 (async () => {
   console.log(await search('Michigun'));
   // { top: 4, stars: 61616, diamonds: 16661, ... }
+
+  // they are equal!
+
+  console.log(await search.user('Michigun'));
+  // { top: 4, stars: 61616, diamonds: 16661, ... }
 })();
