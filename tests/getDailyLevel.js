9fdb55fd5b254051b5a73745d951eecf2bde4ef7
nitrojs <n1trojscoding@mail.ru>
2018-10-27 21:25:58 +0300

    tests: removed

--- tests/getDailyLevel.js
+++
@@ -1,6 +0,0 @@
-const getDaily = require('../functions/getDailyLevel');
-
-(async function getDailyLevel() {
-  console.log(await getDaily());
-  // { daily: 675, name: 'Oscillation', creator: 'GUYdurian', ... }
-})();
--- tests/getWeeklyDemon.js
+++
@@ -1,5 +0,0 @@
-const getWeekly = require('../functions/getWeeklyDemon');
-
-(async function getWeeklyDemon() {
-  console.log(await getWeekly());
-})();
