15e097142feb70565ce5cb04b7d3050b365e9d14
nitrojs <n1trojscoding@mail.ru>
2018-10-29 20:21:36 +0300

    functions: optimized getting daily level and weekly demon

--- functions/getDailyLevel.js
+++ functions/getDailyLevel.js
@@ -6,22 +6,18 @@ async function getDailyLevel() {
   let result = await (await fetch('https://gdprofiles.com/')).text();
   let $ = cheerio.load(result);
   let daily = $('div.col-sm-6.top10').eq(0);
-  let [, dailyNum] = daily.find('h3').text().match(/Daily Level\n#(.*)\n/i);
   let level = daily.find('h3').eq(1).find('a');
-  let levelName = level.eq(0).text();
-  let creator = level.eq(1).text();
   let diffImg = daily.find('.leveldifficon').attr('src');
   let { diff, featured, epic } = getDiff(diffImg);
-  let timestamp = +new Date(daily.find('ul.list-unstyled li').eq(0).text());
 
   return {
-    daily: +dailyNum,
-    name: levelName,
-    creator,
+    daily: +daily.find('h3').text().match(/Daily Level\n#(.*)\n/i)[1],
+    name: level.eq(0).text(),
+    creator: level.eq(1).text(),
     diff,
     featured,
     epic,
-    timestamp,
+    timestamp: +new Date(daily.find('ul.list-unstyled li').eq(0).text()),
   };
 }
 
--- functions/getWeeklyDemon.js
+++ functions/getWeeklyDemon.js
@@ -6,19 +6,17 @@ async function getWeeklyDemon() {
   let result = await (await fetch('https://gdprofiles.com/')).text();
   let $ = cheerio.load(result);
   let weekly = $('div.col-sm-6.top10').eq(1);
-  let [, weeklyNum] = weekly.find('h3').text().match(/Weekly demon\n#(.*)\n/);
   let level = weekly.find('h3 a');
   let { diff, featured, epic } = getDiff(weekly.find('.leveldifficon').attr('src'));
-  let timestamp = +new Date(weekly.find('ul.list-unstyled li').eq(0).text());
 
   return {
-    weekly: +weeklyNum,
+    weekly: +weekly.find('h3').text().match(/Weekly demon\n#(.*)\n/)[1],
     name: level.eq(0).text(),
     creator: level.eq(1).text(),
     diff,
     featured,
     epic,
-    timestamp,
+    timestamp: +new Date(weekly.find('ul.list-unstyled li').eq(0).text()),
   };
 }
 
