96ce125f4f7ee78837f601ea45f7019c343ea7b8
nitrojs <n1trojscoding@mail.ru>
2018-10-31 19:34:57 +0300

    added getTopCreators function

---
+++ functions/getTopCreators.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch');
+let cheerio = require('cheerio');
+
+async function getTopCreators() {
+  let result = await (await fetch('https://gdprofiles.com/')).text();
+  let $ = cheerio.load(result);
+
+  return $('div.col-sm-6.top10').eq(3).find('li').get()
+    .map((elem) => {
+      let data = $(elem).find('h3');
+      let img = data.find('.top_playericon').attr('src');
+      let [, top, nick, cp] = data.text().match(/([0-9]+)(.*)\n+([0-9]+)/);
+
+      return {
+        top: +top,
+        nick,
+        cp: +cp,
+        img,
+        mod: !!data.find('.mod_badge img').toString(),
+        linked: !!data.find('.badge').toString(),
+      };
+    });
+}
+
+module.exports = getTopCreators;
